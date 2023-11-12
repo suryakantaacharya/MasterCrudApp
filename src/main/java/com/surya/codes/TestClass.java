dde3a3949cac0bd6c95c3224b19251e1a463023b
SuryakantaAcharya <bubu.brl.sa@gmail.com>
2023-11-12 11:35:12 +0530

    test

---
+++ src/main/java/com/surya/codes/TestClass.java
@@ -0,0 +1,23 @@
+package com.surya.codes;
+
+public class TestClass {
+	
+	
+	/**
+	 * Returns a working fine msg.
+	 * 
+	 * @return
+	 */
+	public String testSurya() {
+		return "working fine";
+	}
+	
+	/**
+	 * Returns a hello message.
+	 * 
+	 * @return
+	 */
+	public String hello() {
+		return "hi boss";
+	}
+}
